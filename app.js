f1d58e1a2c4af610bef06e745aa25aa0dd5118c1
Gustav-Smith <gustavojeanpt@gmail.com>
2024-01-10 19:11:14 -0300

    Finished

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require("express")
-const rotaLivro = require("./rotas/livro")
+const express = require("express");
+const rotaLivro = require("./rotas/livro");
+const rotaFavorito = require("./rotas/favorito");
 
-const app = express()
-app.use(express.json())
+const cors = require("cors");
 
-app.use('/livros', rotaLivro)
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-const port = 8000
+app.use('/livros', rotaLivro);
+app.use('/favoritos', rotaFavorito)
+
+const port = 8000;
 
 app.listen(port, () => {
-  console.log(`Escutando a porta ${port}`)
-})
+  console.log(`Escutando a porta ${port}`);
+});
--- servicos/livro.js
+++ servicos/livro.js
@@ -3,14 +3,12 @@ const fs = require("fs")
 function getTodosLivros() {
   return JSON.parse(fs.readFileSync("livros.json"))
 }
-
 function getLivroPorId(id) {
   const livros = JSON.parse(fs.readFileSync("livros.json"))
 
   const livroFiltrado = livros.filter(livro => livro.id === id)[0]
   return livroFiltrado
 }
-
 function insereLivro(livroNovo) {
   const livros = JSON.parse(fs.readFileSync("livros.json"))
 
@@ -18,7 +16,6 @@ function insereLivro(livroNovo) {
 
   fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
-
 function modificaLivro(modificacoes, id) {
   let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
   const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
@@ -29,7 +26,6 @@ function modificaLivro(modificacoes, id) {
 
   fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
 }
-
 function deletaLivroPorId(id) {
   const livros = JSON.parse(fs.readFileSync("livros.json"))
 
